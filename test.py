c4f95c916456615ffdd5014a6a6d343a5bc726f2
Ana Silva <as.anasilva@outlook.com>
2018-05-24 15:17:34 -0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,36 @@
+import os
+import nltk.data
+from tkinter import filedialog
+from tkinter import *
+
+_sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+
+def split_sentence(filename, text):
+    sentences = _sent_detector.tokenize(text)
+    needle = 0
+    base=os.path.basename(filename)
+    newfilename = os.path.splitext(base)[0] + "_sentences.txt"
+    
+    for sent in sentences:
+        start = text.find(sent, needle)
+        end = start + len(sent) - 1
+        needle += len(sent)
+
+        file = open(newfilename, 'r', encoding="utf-8")
+        content = file.readlines()
+        content.append(sent + "\n")
+        content.append(str(start) + "\n")
+        content.append(str(end) + "\n")
+        content.append(str(len(sent)-1) + "\n")
+        content.append("----------\n")
+        file = open(newfilename, 'w', encoding="utf-8")
+        file.writelines(content)
+        file.close()
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("Text files","*.txt"),("All files","*.*")))
+    fp = open(root.filename)
+    data = fp.read()
+    split_sentence(root.filename, data)
