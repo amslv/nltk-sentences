f61b7353611c7c4d2d9bd50249be354c5308e62d
Ana Silva <as.anasilva@outlook.com>
2018-05-29 10:21:56 -0300

    up

--- test.py
+++ test.py
@@ -5,18 +5,25 @@ from tkinter import *
 
 _sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 
+def create_file(newfilename):
+    file = open(newfilename, 'w', encoding="utf-8")
+    file.close()
+
+
 def split_sentence(filename, text):
     sentences = _sent_detector.tokenize(text)
     needle = 0
     base=os.path.basename(filename)
     newfilename = os.path.splitext(base)[0] + "_sentences.txt"
+
+    create_file(newfilename)
     
     for sent in sentences:
         start = text.find(sent, needle)
         end = start + len(sent) - 1
         needle += len(sent)
-
         file = open(newfilename, 'r', encoding="utf-8")
+
         content = file.readlines()
         content.append(sent + "\n")
         content.append(str(start) + "\n")
